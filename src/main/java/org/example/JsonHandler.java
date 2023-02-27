39f51714d8c3b9859ee7316fb23ee089f958d438
Oliver Galusinski <64497107+OliverGalusinski@users.noreply.github.com>
2023-02-27 10:12:51 +0100

    Added JSON Can now write Logs on JSON-File
    (Still have to add some formatting, so it's clean)

--- src/main/java/org/example/JsonHandler.java
+++ src/main/java/org/example/JsonHandler.java
@@ -1,6 +1,68 @@
 package org.example;
 
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class JsonHandler {
     // Creates and Handles JSON Files.
     // Also Implements HTTP Send request to the Server.
+    private String containerID;
+    public JsonHandler(String containerID){
+        this.containerID = containerID;
+        this.createJson(containerID);
+    }
+
+    public void createJson(String filePath){
+        try{
+            File file = new File(filePath + ".json");
+            if (!file.exists()){
+                file.createNewFile();
+                System.out.println("Created");
+            }
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public boolean writeListIntoFile(List<String> toWrite) {
+        String fileName = containerID + ".json";
+        try{
+            //Read File
+            StringBuilder jsonString = new StringBuilder();
+            BufferedReader br = new BufferedReader(new FileReader(fileName));
+            String line;
+            while ((line = br.readLine()) != null) {
+                jsonString.append(line);
+            }
+            br.close();
+            JSONObject logData = new JSONObject();
+            if(!jsonString.isEmpty()){
+                JSONParser parser = new JSONParser();
+                System.out.println(jsonString.toString());
+                logData = (JSONObject) parser.parse(jsonString.toString());
+            }
+
+
+
+            FileWriter writer = new FileWriter(fileName);
+            JSONObject finalLogData = logData;
+            toWrite.forEach(singleLog -> {
+                finalLogData.put(singleLog, 1);
+            });
+
+            writer.write(finalLogData.toJSONString());
+            writer.flush();
+            writer.close();
+        } catch (IOException ioException){
+            return false;
+        } catch (ParseException e) {
+            return false;
+        }
+        return true;
+    }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,39 +1,7 @@
 package org.example;
-import com.github.dockerjava.api.DockerClient;
-import com.github.dockerjava.api.model.Container;
-import com.github.dockerjava.core.DockerClientConfig;
-import com.github.dockerjava.core.DefaultDockerClientConfig;
-import com.github.dockerjava.core.DockerClientImpl;
-import com.github.dockerjava.httpclient5.ApacheDockerHttpClient;
-import com.github.dockerjava.transport.DockerHttpClient;
-
-import java.time.Duration;
-import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        /*
-        DockerClientConfig config = DefaultDockerClientConfig.createDefaultConfigBuilder().build();
-        DockerHttpClient httpClient = new ApacheDockerHttpClient.Builder()
-                .dockerHost(config.getDockerHost())
-                .sslConfig(config.getSSLConfig())
-                .maxConnections(100)
-                .connectionTimeout(Duration.ofSeconds(30))
-                .responseTimeout(Duration.ofSeconds(45))
-                .build();
-
-        DockerClient dockerClient = DockerClientImpl.getInstance(config, httpClient);
-        List<Container> containers = dockerClient.listContainersCmd().exec();
-
-        for(Container container : containers){
-            System.out.println("The container " + container.getNames()[0] + " with the ID " + container.getId() + " logged:");
-            List<String> logs = GetLogs.getLogs(dockerClient, container.getId(), 1677166678);
-            for(String log : logs){
-                System.out.println(log);
-            }
-        }
-         */
-
         CheckForNewContainer checkForNewContainer = new CheckForNewContainer();
         checkForNewContainer.setName("CheckForNewContainer-Thread");
         checkForNewContainer.start();
